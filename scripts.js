42035354b6683d1f468b325d9b0c3a220fc1db9c
Sara <amiri_gs@hotmail.com>
2021-10-20 00:00:48 -0400

    added animation

--- scripts.js
+++ scripts.js
@@ -2,11 +2,30 @@
 // Use querySelector to cache arrow divs
 // Add event listener so on click, it scrolls to next section
 
-const portfolio = {};
 
-portfolio.arrows = document.querySelectorAll('.arrow');
+const navigationBar = document.querySelector('.navigationBar');
+const openNav = document.querySelector('.openNav');
+const closeNav = document.querySelector('.closeNav');
 
-portfolio.arrows.forEach((arrow) => {
+// window.addEventListener('scroll', function() {
+//     navigationBar.classList.toggle('sticky', window.scrollY > 0);
+// })
+
+function visibleMenu() {
+    navigationBar.classList.add('responsiveBar');
+}
+
+function unvisibleMenu() {
+    navigationBar.classList.remove('responsiveBar');
+}
+
+openNav.addEventListener('click', visibleMenu);
+
+closeNav.addEventListener('click', unvisibleMenu);
+
+const arrows = document.querySelectorAll('.arrow');
+
+arrows.forEach((arrow) => {
     arrow.addEventListener('click', function() {
         if (this.classList.contains('aboutArrow')) {
             const aboutSection = document.querySelector('.aboutContainer');
@@ -21,9 +40,3 @@ portfolio.arrows.forEach((arrow) => {
         // }
     })
 })
-
-portfolio.init = function () {
-
-};
-
-portfolio.init();
