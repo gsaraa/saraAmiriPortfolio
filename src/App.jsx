fa4acb36c17564ab3db9f990890574bed312044d
Sara <amiri.gs@hotmail.com>
2023-12-04 17:37:33 -0500

    Creates files to convert portfolio to React

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/sass/styles.scss';
+import LandingPage from "./components/LandingPage";
+
+const App = () => {
+	return (
+        <>
+            <LandingPage />
+        </>
+    )
+};
+export default App;
