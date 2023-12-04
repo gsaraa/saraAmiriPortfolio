bc6c604a11c87f9b3c6c2b66afb8c91d10101d76
Sara <amiri.gs@hotmail.com>
2023-12-04 17:56:35 -0500

    Imports Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import '../styles/sass/styles.scss';
 import LandingPage from "./components/LandingPage";
+import Footer from "./components/Footer";
 
 const App = () => {
 	return (
         <>
             <LandingPage />
+            <Footer />
         </>
     )
 };
