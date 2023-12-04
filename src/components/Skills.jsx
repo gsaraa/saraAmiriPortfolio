98eb6125595ac598fb9ce0c786edb83e84fe5dbe
Sara <amiri.gs@hotmail.com>
2023-12-04 17:59:19 -0500

    Creates Skills component

---
+++ src/components/Skills.jsx
@@ -0,0 +1,79 @@
+import React from "react";
+
+const Skills = () => {
+	return (
+		<>
+			<section class="skillsSection" id="skills">
+				<h2>My Skills</h2>
+				<ul class="skillsList">
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="devicon-html5-plain"></i>
+						<p>html</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="devicon-css3-plain"></i>
+						<p>css</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="devicon-sass-original"></i>
+						<p>sass</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="devicon-javascript-plain"></i>
+						<p>javascript</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="devicon-react-original"></i>
+						<p>react</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="devicon-jquery-plain"></i>
+						<p>jquery</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="devicon-firebase-plain"></i>
+						<p>firebase</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="fas fa-terminal"></i>
+						<p>command line</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="devicon-github-original"></i>
+						<p>github</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="devicon-git-plain"></i>
+						<p>git</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="fas fa-universal-access"></i>
+						<p>Accessibility</p>
+					</li>
+
+					<li data-aos="flip-down" data-aos-duration="900">
+						<i class="fas fa-mobile-alt"></i>
+						<p>Responsive design</p>
+					</li>
+
+					<li data-aos="flip-up" data-aos-duration="900">
+						<i class="fas fa-cogs"></i>
+						<p>api</p>
+					</li>
+				</ul>
+			</section>
+		</>
+	);
+};
+
+export default Skills;
