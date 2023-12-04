ea3095d35b8386710a052023304b4b3bd9b50ded
Sara <amiri.gs@hotmail.com>
2023-12-04 17:59:33 -0500

    Create Footer component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const Footer = () => {
+	return (
+		<>
+			<div class="arrowUp">
+				<i class="fas fa-angle-double-up"></i>
+			</div>
+
+			<footer>
+				<p>Designed and developed by Sara Amiri</p>
+			</footer>
+		</>
+	);
+};
+
+export default Footer;
