4d8bc6d0392ee1cefec2280b6f13dee204a953fa
Sara <amiri.gs@hotmail.com>
2024-01-23 17:06:18 -0500

    Removes imported components and moves them to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from "react";
 import '../styles/sass/styles.scss';
 import LandingPage from "./components/LandingPage";
+import NavBar from "./components/NavBar";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Skills from "./components/Skills";
+import Form from "./components/Form";
 import Footer from "./components/Footer";
 
 const App = () => {
 	return (
         <>
             <LandingPage />
+            <NavBar />
             <Footer />
         </>
     )
--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,34 +1,25 @@
 import React from "react";
 
-import NavBar from "./NavBar";
-import AboutMe from "./AboutMe";
-import Projects from "./Projects";
-import Skills from "./Skills";
-import Form from "./Form";
+import me from "../../public/assets/me.jpg";
 
 const LandingPage = () => {
-	// useEffect(() => {
-	// 	AOS.init();
-	// }, []);
-
 	return (
-		<div class="wrapper">
-			<h1 data-aos="zoom-in-right" data-aos-duration="800">
-				Sara Amiri
-			</h1>
+		<div class="header">
+			<div class="aboutImg">
+				<img src={me} alt="A photo of Sara Amiri" />
+			</div>
+			<div class="wrapper title">
+				<h1 data-aos="zoom-in-right" data-aos-duration="800">
+					Sara Amiri
+				</h1>
 
-			<p
-				class="developer"
-				data-aos="zoom-in-left"
-				data-aos-duration="800">
-				Front-end Developer
-			</p>
-
-			<NavBar />
-			<AboutMe />
-			<Projects />
-			<Skills />
-			<Form />
+				<p
+					class="developer"
+					data-aos="zoom-in-left"
+					data-aos-duration="800">
+					Front-end Developer
+				</p>
+			</div>
 		</div>
 	);
 };
